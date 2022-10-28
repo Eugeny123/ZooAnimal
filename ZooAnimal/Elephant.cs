9030db9e09874cfce14d4e87d2c880424156e974
Eugeny <sgenya72@gmail.com>
2022-10-28 13:11:33 +0300

    added beautiful ))) in class Elephant

--- ZooAnimal/Elephant.cs
+++ ZooAnimal/Elephant.cs
@@ -10,16 +10,19 @@ namespace ZooAnimal
     {
         public string Name;
         public int EarSize;
+
         public void WhoAml()
         {
             Console.WriteLine("My name is " + Name+" .");
             Console.WriteLine("My ears are " + EarSize + " inches tall.");
         }
+
         public void HearMessage(string message, Elephant whoSaidIt)
         {
             Console.WriteLine(Name + " heard a message");
             Console.WriteLine(whoSaidIt.Name + " said this: "+ message);
         }
+
         public void SpeakTo(Elephant whoToTalkTo, string message)
         {
             whoToTalkTo.HearMessage(message, this);
